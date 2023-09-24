ec47d92ab33bb7c2f421975806ac27f7151ec556
Adrian <jadrianguillermo@gmail.com>
2023-09-24 18:23:22 +0800

    Login, register, and firebase auth setup
    - added pages for login and register page
    - initialized firebase setup using dotenv
    - added firebase auth integration on login, register page and logout button
    - added displayName from LocalStorage user added during login

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,39 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+   <q-header elevated>
+    <q-toolbar>
+     <q-toolbar-title>
+      Quasar App
+     </q-toolbar-title>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+     <q-toolbar-subtitle v-if="displayName">Welcome, {{ displayName }}!</q-toolbar-subtitle>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+     <q-btn flat @click="logout">Logout</q-btn>
+    </q-toolbar>
+   </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+   <q-page-container>
+    <router-view />
+   </q-page-container>
   </q-layout>
-</template>
+ </template>
 
-<script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+<script setup>
+import signout from 'src/firebase/firebase-signout'
+import { useRouter } from 'vue-router'
+import { LocalStorage } from 'quasar'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+const getDisplayNameFromLocalStorage = () => {
+  const user = LocalStorage.getItem('user')
+  return user ? user.displayName : null
+}
 
-export default defineComponent({
-  name: 'MainLayout',
+const displayName = getDisplayNameFromLocalStorage()
 
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+const router = useRouter()
+const logout = () => {
+  signout().then(() => {
+    router.push('/login')
+  })
+}
 </script>
