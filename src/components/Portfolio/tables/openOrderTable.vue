66557e4213986e438138184b5559b1f823ff88b7
devModeNow <ronaldfernando.dev@gmail.com>
2023-10-16 21:05:48 +0800

    Minor tweaks on Portfolio page UI

--- src/components/Portfolio/tables/openOrderTable.vue
+++ src/components/Portfolio/tables/openOrderTable.vue
@@ -41,157 +41,157 @@
 </template>
 
 <script>
-import moment from 'moment';
+import moment from 'moment'
 
 export default {
-    name: 'PeriodTable',
-    data(){
-        return {
-            tableRow: [
-                {
-                    period: 'Today',
-                    gain: '+0.0%',
-                    gainDif: '+1.23%',
-                    profit: '$0.0',
-                    profitDif: '+$40.23',
-                    pips: '0.00',
-                    pipsDif: '+15.2',
-                    win: '0%',
-                    winDif: '-66%',
-                    trade: '0',
-                    tradeDif: '-2',
-                    lots: '0.00',
-                    lotsDif: '-0.65',
-                },
-                {
-                    period: 'This Week',
-                    gain: '-0.0%',
-                    gainDif: '+1.23%',
-                    profit: '-$1.3',
-                    profitDif: '+$40.23',
-                    pips: '0.00',
-                    pipsDif: '+15.2',
-                    win: '0%',
-                    winDif: '-66%',
-                    trade: '0',
-                    tradeDif: '-2',
-                    lots: '0.00',
-                    lotsDif: '-0.65',
-                },
-                {
-                    period: 'This Month',
-                    gain: '+0.0%',
-                    gainDif: '+1.23%',
-                    profit: '+$2.0',
-                    profitDif: '+$40.23',
-                    pips: '0.00',
-                    pipsDif: '+15.2',
-                    win: '0%',
-                    winDif: '-66%',
-                    trade: '0',
-                    tradeDif: '-2',
-                    lots: '0.00',
-                    lotsDif: '-0.65',
-                },
-                {
-                    period: 'This Year',
-                    gain: '+0.0%',
-                    gainDif: '+1.23%',
-                    profit: '$0.0',
-                    profitDif: '+$40.23',
-                    pips: '0.00',
-                    pipsDif: '+15.2',
-                    win: '0%',
-                    winDif: '-66%',
-                    trade: '0',
-                    tradeDif: '-2',
-                    lots: '-0.00',
-                    lotsDif: '-0.65',
-                },
-            ],
+  name: 'PeriodTable',
+  data () {
+    return {
+      tableRow: [
+        {
+          period: 'Today',
+          gain: '+0.0%',
+          gainDif: '+1.23%',
+          profit: '$0.0',
+          profitDif: '+$40.23',
+          pips: '0.00',
+          pipsDif: '+15.2',
+          win: '0%',
+          winDif: '-66%',
+          trade: '0',
+          tradeDif: '-2',
+          lots: '0.00',
+          lotsDif: '-0.65'
+        },
+        {
+          period: 'This Week',
+          gain: '-0.0%',
+          gainDif: '+1.23%',
+          profit: '-$1.3',
+          profitDif: '+$40.23',
+          pips: '0.00',
+          pipsDif: '+15.2',
+          win: '0%',
+          winDif: '-66%',
+          trade: '0',
+          tradeDif: '-2',
+          lots: '0.00',
+          lotsDif: '-0.65'
+        },
+        {
+          period: 'This Month',
+          gain: '+0.0%',
+          gainDif: '+1.23%',
+          profit: '+$2.0',
+          profitDif: '+$40.23',
+          pips: '0.00',
+          pipsDif: '+15.2',
+          win: '0%',
+          winDif: '-66%',
+          trade: '0',
+          tradeDif: '-2',
+          lots: '0.00',
+          lotsDif: '-0.65'
+        },
+        {
+          period: 'This Year',
+          gain: '+0.0%',
+          gainDif: '+1.23%',
+          profit: '$0.0',
+          profitDif: '+$40.23',
+          pips: '0.00',
+          pipsDif: '+15.2',
+          win: '0%',
+          winDif: '-66%',
+          trade: '0',
+          tradeDif: '-2',
+          lots: '-0.00',
+          lotsDif: '-0.65'
         }
-    },
-    components: {},
-    created(){},
-    methods: {
-        moment,
-        checkValueColor(val){
-            let positiveMatch = /[+]/gi;
-            let negativeMatch = /[-]/gi;
-            let color = "";
-            if(val.match(positiveMatch)){
-                color = 'text-green-7';
-            } else if(val.match(negativeMatch)){
-                color = 'text-red-7';
-            }
-
-            return color;
-        }
-        // end
-    },
-    computed: {
-        
-        tableColumns: function(){
-            // ["Business Name", "Address", "Category", "Call", "Remarks", "Action"];
-            return [
-                {
-                    name: 'gain',
-                    required: true,
-                    label: 'Gain (Difference)',
-                    align: 'left',
-                    field: row => row.gain,
-                    format: val => `${val}`,
-                    sortable: true
-                },
-                {
-                    name: 'profit',
-                    required: true,
-                    label: 'Profit (Difference)',
-                    align: 'left',
-                    field: row => row.profit,
-                    format: val => `${val}`,
-                    sortable: true
-                },
-                {
-                    name: 'pips',
-                    required: true,
-                    label: 'Pips (Difference)',
-                    align: 'left',
-                    field: row => row.pips,
-                    format: val => `${val}`,
-                    sortable: true
-                },
-                {
-                    name: 'win',
-                    required: true,
-                    label: 'Win % (Difference)',
-                    align: 'left',
-                    field: row => row.win,
-                    format: val => `${val}`,
-                    sortable: true
-                },
-                {
-                    name: 'trade',
-                    required: true,
-                    label: 'Trade (Difference)',
-                    align: 'left',
-                    field: row => row.trade,
-                    format: val => `${val}`,
-                    sortable: true
-                },
-                {
-                    name: 'lots',
-                    required: true,
-                    label: 'Lots (Difference)',
-                    align: 'left',
-                    field: row => row.lots,
-                    format: val => `${val}`,
-                    sortable: true
-                },
-                
-            ]
-        }
+      ]
     }
+  },
+  components: {},
+  created () {},
+  methods: {
+    moment,
+    checkValueColor (val) {
+      const positiveMatch = /[+]/gi
+      const negativeMatch = /[-]/gi
+      let color = ''
+      if (val.match(positiveMatch)) {
+        color = 'text-green-7'
+      } else if (val.match(negativeMatch)) {
+        color = 'text-red-7'
+      }
+
+      return color
+    }
+    // end
+  },
+  computed: {
+
+    tableColumns: function () {
+      // ["Business Name", "Address", "Category", "Call", "Remarks", "Action"];
+      return [
+        {
+          name: 'gain',
+          required: true,
+          label: 'Gain (Difference)',
+          align: 'left',
+          field: row => row.gain,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'profit',
+          required: true,
+          label: 'Profit (Difference)',
+          align: 'left',
+          field: row => row.profit,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'pips',
+          required: true,
+          label: 'Pips (Difference)',
+          align: 'left',
+          field: row => row.pips,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'win',
+          required: true,
+          label: 'Win % (Difference)',
+          align: 'left',
+          field: row => row.win,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'trade',
+          required: true,
+          label: 'Trade (Difference)',
+          align: 'left',
+          field: row => row.trade,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: 'lots',
+          required: true,
+          label: 'Lots (Difference)',
+          align: 'left',
+          field: row => row.lots,
+          format: val => `${val}`,
+          sortable: true
+        }
+
+      ]
+    }
+  }
 }
 </script>
 
