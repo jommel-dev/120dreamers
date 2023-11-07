dac5a11a0e4caf5169f9498dfe9e47c2a54e6907
devModeNow <ronaldfernando.dev@gmail.com>
2023-11-07 14:09:54 +0800

    Account Registration

--- src/firebase/firebase-register.js
+++ src/firebase/firebase-register.js
@@ -1,26 +1,55 @@
-import { auth } from './index.js'
+/* #version=0.0.0-1#1 rf 2023-11-07T12:14:53 400D2C82DFC836CF */
+/* #version=0.0.0-1 rf 2023-11-07T12:12:07 C23ED082845F2AB6 */
+import { auth, db } from './index.js'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { doc, setDoc, collection } from 'firebase/firestore'
 import { Loading, Notify } from 'quasar'
 
 const register = (data) => {
   return new Promise((resolve, reject) => {
     Loading.show()
 
-    createUserWithEmailAndPassword(auth, data.email, data.password).then(userCredential => {
-      updateProfile(userCredential.user, {
-        displayName: data.first_name + ' ' + data.last_name
-      })
+    createUserWithEmailAndPassword(
+      auth,
+      data.email,
+      data.password,
+      data.first_name,
+      data.last_name
+    )
+      .then((userCredential) => {
+        updateProfile(userCredential.user, {
+          displayName: data.first_name + ' ' + data.last_name
+        })
 
-      Loading.hide()
-      resolve(userCredential.user)
-    }).catch(err => {
-      Loading.hide()
-      Notify.create({
-        type: 'negative',
-        message: err.message
+        const user = userCredential.user
+
+        console.log(user.uid)
+
+        // Save user data to Firestore
+        const docRef = doc(db, 'userProfile', user.uid)
+
+        const userData = {
+          firstName: data.first_name,
+          lastName: data.last_name
+          // Add other user-specific data here
+        }
+
+        return setDoc(docRef, userData).then(() => {
+          Loading.hide()
+          resolve(userCredential.user)
+        })
+
+        // Loading.hide()
+        // resolve(userCredential.user)
       })
-      reject(err.message)
-    })
+      .catch((err) => {
+        Loading.hide()
+        Notify.create({
+          type: 'negative',
+          message: err.message
+        })
+        reject(err.message)
+      })
   })
 }
 
