f83bca08292e15cab61134f3da438b2704c8bb7c
Alexander Alfon <87632745+AAlfon@users.noreply.github.com>
2023-11-21 18:48:16 +0800

    New Design for Trading Floor

--- src/components/TradingFloor/index.vue
+++ src/components/TradingFloor/index.vue
@@ -1,7 +1,7 @@
 <template >
   <div id="q-app" class="app-container" style="min-height: 100vh;">
         <div class="q-pa-md example-row-equal-width">
-            <h3>Trading Floor</h3>
+            <h3 style="color:grey;">Trading Floor</h3>
 
             <div class="row">
                 <div class="col">
@@ -57,8 +57,12 @@
 </script>
 <style >
 .app-container {
-    background: rgb(27,27,28);
-background: linear-gradient(56deg, rgba(27,27,28,0.8183648459383753) 0%, rgba(1,55,105,0.6446953781512605) 50%, rgba(27,27,28,0.8407738095238095) 100%);
+    background: url("/imgs/illustration.svg") center no-repeat;
+  /* background-position: 136% 0%; */
+  background-size: cover;
+  background-color: rgba(0, 0, 0, 0.959);
+    /* background: rgb(27,27,28);
+background: linear-gradient(56deg, rgba(27,27,28,0.8183648459383753) 0%, rgba(1,55,105,0.6446953781512605) 50%, rgba(27,27,28,0.8407738095238095) 100%); */
 }
 .example-row-equal-width .row > div {
   padding: 10px 15px;
